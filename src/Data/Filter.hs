292db69426155faa092c10de2921f18a9242b9d3
Tobias Brandy <tbrandy@itba.edu.ar>
2022-08-29 11:38:29 -0300

    Move satellite functions to internal module

--- src/Data/Filter.hs
+++ src/Data/Filter.hs
@@ -6,13 +6,33 @@ module Data.Filter (
 
 import Prelude hiding (exp, seq, any, filter, init)
 
+import Data.Filter.Internal
+  ( FilterRet (..)
+  , retOk
+  , retErr
+  , retToEither
+
+  , FilterResult
+  , foldrRet
+  , resultOk
+  , resultErr
+  , mapMRet
+  , concatMapMRet
+  , mapRet
+  , foldMRet
+
+  , jsonShowError
+
+  , concatMapM
+  )
+
 import Data.Json (Json (..))
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Sequence (Seq ((:|>)))
 import qualified Data.Sequence as Seq
 import qualified Data.HashMap.Strict as Map
-import Data.Foldable (Foldable(foldl', toList), fold)
+import Data.Foldable (Foldable(foldl', toList))
 import Data.Scientific (isInteger, toBoundedInteger)
 import Data.Maybe (fromMaybe)
 import Control.Monad (liftM, ap, liftM2, when, foldM)
@@ -100,7 +120,6 @@ filterRun filter json = let
     (_, ret)      = f filterRunInitState
   in foldrRet ((:) . retToEither) [] ret
 
-
 ------------------------ Internal --------------------------
 
 data FilterRunState = FilterRunState {
@@ -158,106 +177,6 @@ filterRunFuncGet name argc = FilterRun $ \s@FilterRunState { fr_funcs, fr_curren
 
 filterRunIsTopLevel :: FilterRun Bool
 filterRunIsTopLevel = FilterRun $ \s@FilterRunState { fr_file } -> (s, fr_file == TopLevel)
-
-data FilterRet a
-  = Ok      a
-  | Err     Text
-  | Interrupt
-
-instance Functor FilterRet where
-  fmap = liftM
-
-instance Applicative FilterRet where
-  pure = Ok
-  (<*>) = ap
-
-instance Monad FilterRet where
-  m >>= k = case m of
-    Ok a      -> k a
-    Err msg   -> Err msg
-    Interrupt -> Interrupt
-
-retOk :: Monad m => a -> m (FilterRet a)
-retOk = return . Ok
-
-retErr :: Monad m => Text -> m (FilterRet a)
-retErr = return . Err
-
-applyRet :: (a -> t) -> (FilterRet b -> t) -> FilterRet a -> t
-applyRet ok errOrInt ret = case ret of
-  (Ok a)    -> ok a
-  (Err msg) -> errOrInt $ Err msg
-  Interrupt -> errOrInt Interrupt
-
-retToEither :: FilterRet a -> Either Text a
-retToEither (Ok a)    = Right a
-retToEither (Err msg) = Left msg
-retToEither Interrupt = error "Interrupts cannot be mapped to either"
-
-type FilterResult a = [FilterRet a]
-
-resultOk :: Monad m => a -> m (FilterResult a)
-resultOk = return . (:[]) . Ok
-
-resultErr :: Monad m => Text -> m (FilterResult a)
-resultErr = return . (:[]) . Err
-
-foldrRetOnInt :: (FilterRet a -> t -> t) -> t -> t -> FilterResult a -> t
-foldrRetOnInt f base int = foldr go base
-  where
-    go Interrupt  _   = f Interrupt int
-    go x          ret = f x ret
-
-foldrRet :: (FilterRet a -> t -> t) -> t -> FilterResult a -> t
-foldrRet f base = foldrRetOnInt f base base
-
-mapRet :: (a -> FilterRet b) -> FilterResult a -> FilterResult b
-mapRet f = foldrRet go []
-  where
-    go (Ok a) ret = case f a of
-      Interrupt -> [Interrupt]
-      other     -> other : ret
-    go (Err msg)  ret = Err msg : ret
-    go Interrupt  ret = Interrupt : ret
-
-mapRet' :: (a -> b) -> FilterResult a -> FilterResult b
-mapRet' f = foldrRet ((:) . applyRet (Ok . f) id) []
-
-foldMRet :: Monad m => (b -> a -> m (FilterRet b)) -> FilterRet b -> FilterResult a -> m (FilterRet b)
-foldMRet f = foldMRet' run
-  where
-    run _         (Err msg) = return $ Err msg
-    run _         Interrupt = return Interrupt
-    run (Ok ret)  (Ok x)    = f ret x
-    run (Err _)   _         = invalidStateErr "Err"
-    run Interrupt _         = invalidStateErr "Interrupt"
-
-    invalidStateErr retCase = error $ "ret=" <> retCase <> ": Should never happen. Case 1 already catches this case. We want to short circuit on errors."
-
-foldMRet' :: Monad m => (t -> FilterRet a -> m t) -> t -> FilterResult a -> m t
-foldMRet' f base xs = foldrRet go return xs base
-  where go x fret acum = fret =<< f acum x
-
-mapMRet :: Monad m => (a -> m (FilterRet b)) -> FilterResult a -> m (FilterResult b)
-mapMRet f = foldMRet' go []
-  where
-    go ret (Ok a) = do
-      b <- f a
-      case b of
-        Interrupt -> return [Interrupt]
-        other     -> return $ other : ret
-    go ret (Err msg)  = pure $ Err msg : ret
-    go _   Interrupt  = pure [Interrupt]
-
-mapMRet' :: Monad m => (a -> m b) -> FilterResult a -> m (FilterResult b)
-mapMRet' f = sequence . foldrRet ((:) . applyRet (fmap Ok . f) return) []
-
-concatRet :: FilterResult (FilterResult a) -> FilterResult a
-concatRet = foldrRet (\x ret -> applyRet (foldrRetOnInt (:) ret []) (:ret) x) []
-
-concatMapMRet :: Monad m => (a -> m (FilterResult b)) -> FilterResult a -> m (FilterResult b)
-concatMapMRet f = fmap concatRet . mapMRet' f
-
 
 -- TODO(tobi): Si agregamos modulos, agregar en que file paso el error/no esta defninida la cosa (como jq)
 runFilter :: Filter -> Json -> FilterRun (FilterResult Json)
@@ -483,7 +402,7 @@ runReduce exp name initial update json = do
       else
         return $ last <$> sequence newRet
 
--- TOD)(tobi): Malisimo ir concatenando. Se podria usar dlist, pero habria que usarlas siempre para que valga la pena.
+-- TODO(tobi): Malisimo ir concatenando. Se podria usar dlist, pero habria que usarlas siempre para que valga la pena.
 runForeach :: Filter -> Text -> Filter -> Filter -> Filter -> Json -> FilterRun (FilterResult Json)
 runForeach exp name initial update extract json = do
   stream  <- runFilter exp json
@@ -555,17 +474,3 @@ runFunc state params body args json = do
 
 runLOC :: Integral a => Text -> a -> FilterRun (FilterResult Json)
 runLOC file line = resultOk $ Object $ Map.fromList [("file", String file), ("line", Number $ fromIntegral line)]
-
--- Error handling --
--- TODO(tobi): Agregar cantidad maxima de caracteres y luego ...
-jsonShowError :: Json -> Text
-jsonShowError json@(Number  _)  = "number ("  <> showt json <> ")"
-jsonShowError json@(String  _)  = "string ("  <> showt json <> ")"
-jsonShowError json@(Bool    _)  = "boolean (" <> showt json <> ")"
-jsonShowError json@(Object  _)  = "object ("  <> showt json <> ")"
-jsonShowError json@(Array   _)  = "array ("   <> showt json <> ")"
-jsonShowError json@Null         = "null ("    <> showt json <> ")"
-
--- Utils --
-concatMapM :: (Monoid b, Traversable t, Applicative f) => (a -> f b) -> t a -> f b
-concatMapM f = fmap fold . traverse f
