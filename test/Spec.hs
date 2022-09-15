d70293999cd985f4ebf8dd485f3fa7514b2cde13
Tobias Brandy <tbrandy@itba.edu.ar>
2022-09-15 00:44:22 -0300

    Tests: first step

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,45 +1,81 @@
-import Prelude hiding (filter)
+{-# LANGUAGE TemplateHaskell #-}
 
 import Test.HUnit (runTestTT, Test (..), assertEqual, showCounts)
 
-import Data.Filter (runFilter, Filter (..))
+import Data.Filter (Filter (..))
+import Data.Filter.Run (filterRunExp)
 import Data.Json (Json (..))
 
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as BSS
 import Parse.Defs (parserStateInit, parserRun, ParserResult (..))
 import Parse.Json.Parser (jsonParser)
 import Parse.Filter.Parser (filterParser)
+import Data.Maybe (fromJust)
+import Data.FileEmbed (embedFile)
+import Data.Word (Word8)
+import Data.List (groupBy)
+import Data.Filter.Builtins (builtins)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Function ((&))
 
+newLine :: Word8
+newLine = 10
+
+hashtag :: Word8
+hashtag = 35
 
 main :: IO ()
 main = do
-  counts <- runTestTT $ TestList tests
+  counts <- runTestTT $ TestList jqTests
   putStrLn $ showCounts counts
 
-tests :: [Test]
-tests = [
-  TestLabel "testObjectFilter" $ TestCase $
-    assertEqual "null -> {'hola': 1}" 
-    [ json "{\"1\": \"1\"}"
-    , json "{\"1\": \"2\"}"
-    , json "{\"1\": \"3\"}"
-    , json "{\"2\": \"1\"}"
-    , json "{\"2\": \"2\"}"
-    , json "{\"2\": \"3\"}"
-    , json "{\"3\": \"1\"}"
-    , json "{\"3\": \"2\"}"
-    , json "{\"3\": \"3\"}"
-    ] $
-    runFilter (filter "{(.[]): .[]}") (json "[\"1\",\"2\", \"3\"]")
-  ]
+jqTests :: [Test]
+jqTests
+  = $(embedFile "test/jq.test")
+  & zip [0..] . BSS.split newLine
+  & filter (\(_, s) -> BSS.null s || BSS.head s /= hashtag)
+  & groupBy (\(_, l) (_, r) -> let nl = BSS.null l; nr = BSS.null r in (nl && nr) || not (nl || nr))
+  & filter (not . BSS.null . snd . head)
+  & map (\((i, x):xs) -> testDefToTest (i, BS.fromStrict x : map (BS.fromStrict . snd) xs))
 
-json :: ByteString -> Json
-json input = parserOkResult $ parserRun (parserStateInit input) jsonParser
+testDefToTest :: (Int, [ByteString]) -> Test
+testDefToTest (line, program : input : output) = TestCase $
+  assertEqual ("Test line " <> show line)
+    (map errToString $ filterRunExp builtins (parseFilter program) (parseJson input))
+    (map parseJson output)
+testDefToTest xs = error $ "Malformed test: " <> show xs
 
-filter :: ByteString -> Filter
-filter input = parserOkResult $ parserRun (parserStateInit input) filterParser
+-- tests :: [Test]
+-- tests = [
+--   TestLabel "testObjectFilter" $ TestCase $
+--     assertEqual "null -> {'hola': 1}" 
+--     [ json "{\"1\": \"1\"}"
+--     , json "{\"1\": \"2\"}"
+--     , json "{\"1\": \"3\"}"
+--     , json "{\"2\": \"1\"}"
+--     , json "{\"2\": \"2\"}"
+--     , json "{\"2\": \"3\"}"
+--     , json "{\"3\": \"1\"}"
+--     , json "{\"3\": \"2\"}"
+--     , json "{\"3\": \"3\"}"
+--     ] $
+--     rights $ filterRunExp Map.empty (filter "{(.[]): .[]}") (json "[\"1\",\"2\", \"3\"]")
+--   ]
+
+errToString :: Either Text Json -> Json
+errToString (Left msg) = String msg
+errToString (Right json) = json
+
+parseJson :: ByteString -> Json
+parseJson input = fromJust $ parserOkResult $ parserRun (parserStateInit input) jsonParser
+
+parseFilter :: ByteString -> Filter
+parseFilter input = parserOkResult $ parserRun (parserStateInit input) filterParser
 
 parserOkResult :: ParserResult token result -> result
 parserOkResult (Ok (_, ret)) = ret
-parserOkResult _ = error "Error during parsing"
+parserOkResult (Error msg) = error $ "Error during parsing: " <> T.unpack msg
 
