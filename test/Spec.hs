c99b1c2734128ae74c3ecbbbb5afcf3cf6c1209b
Tobias Brandy <tbrandy@itba.edu.ar>
2022-09-21 15:13:26 -0300

    Test: Allow multi json input and output

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,17 +2,15 @@
 
 import Test.HUnit (Test (..), runTestTTAndExit, assertEqual)
 
-import Data.Filter (Filter (..))
+import Data.Filter (Filter)
 import Data.Filter.Run (filterRunExp)
-import Data.Json (Json (..))
+import Data.Json (Json)
 
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString as BSS
 import Parse.Defs (parserStateInit, parserRun, ParserResult (..))
-import Parse.Json.Parser (jsonParser)
 import Parse.Filter.Parser (filterParser)
-import Data.Maybe (fromJust)
 import Data.FileEmbed (embedFile)
 import Data.List (groupBy)
 import Data.Filter.Builtins (builtins)
@@ -21,6 +19,8 @@ import qualified Data.Text as T
 import Data.Function ((&))
 import Data.Text.Encoding.Error (lenientDecode)
 import Data.Text.Encoding (decodeUtf8With)
+import Lib (repl)
+import Data.Functor.Identity (runIdentity, Identity (..))
 
 main :: IO ()
 main = runTestTTAndExit $ TestList [baseTests, manualTests]
@@ -46,23 +46,21 @@ parseTests file = TestList
 testDefToTest :: (Int, [ByteString]) -> Test
 testDefToTest (line, program : input : output) = TestCase $
   assertEqual ("Line " <> show line <> "; echo '" <> showBS input <> "' | jqhs '" <> showBS program <> "'")
-    (map parseJson output)
-    (map failOnErr $ filterRunExp builtins (parseFilter program) (parseJson input))
+    (concatMap parseJson output)
+    (map failOnErr $ concatMap (filterRunExp builtins (parseFilter program)) (parseJson input))
 testDefToTest xs = error $ "Malformed test: " <> show xs
 
 failOnErr :: Either Text Json -> Json
 failOnErr (Left msg) = error $ T.unpack msg
 failOnErr (Right json) = json
 
-parseJson :: ByteString -> Json
-parseJson input = fromJust $ parserOkResult $ parserRun (parserStateInit input) jsonParser
+parseJson :: ByteString -> [Json]
+parseJson input = reverse $ runIdentity $ repl (error . ("Error during json parsing: " <>) . T.unpack) (\js -> Identity . (:js)) [] (parserStateInit input)
 
 parseFilter :: ByteString -> Filter
-parseFilter input = parserOkResult $ parserRun (parserStateInit input) filterParser
-
-parserOkResult :: ParserResult token result -> result
-parserOkResult (Ok (_, ret)) = ret
-parserOkResult (Error msg) = error $ "Error during parsing: " <> T.unpack msg
+parseFilter input = case parserRun (parserStateInit input) filterParser of
+  Ok (_, ret) -> ret
+  Error msg   -> error $ "Error during filter parsing: " <> T.unpack msg
 
 showBS :: ByteString -> String
 showBS = T.unpack  . decodeUtf8With lenientDecode . BS.toStrict
